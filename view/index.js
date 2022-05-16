d69a12e1aa24f1b6fb2a40b7bf981b50f5a24b2f
Akoneh Silas <silasmagho18@gmail.com>
2022-05-16 17:45:28 +0100

    version 1.2

--- view/index.js
+++ view/index.js
@@ -3,7 +3,7 @@
 // let cell = document.createTextNode("");
 
 
-// let board = Array.from(Array(50), () => new Array(50));
+// let board = Array.from(Array(numOfRows), () => new Array(numOfRows));
 
 
 // for(let i=0; i<board.length; i++){
@@ -21,117 +21,201 @@
 // }
 
 // ``
+//create array of iDs from 1 to numOfCells//___________________________________________________________________________________________
 let id = 1;
+let whileTrack = 1
 let cellIds = []
+let numOfRows = 50;
+let numOfRowsCurr = numOfRows + 2;
+let numOfCells = numOfRows**2
+let numOfCellsCurr = numOfRowsCurr**2
 
-for(let i = 0; i < 50;i++){
-    for(let j = 0; j < 50; j++){
+for(let i = 0; i < numOfRows;i++){
+    for(let j = 0; j < numOfRows; j++){
         cellIds.push(id);
         id++
     }
     
 }
+//Dynamicaly create numOfRows by numOfRows table
 console.log(cellIds)
 let cells = ""
-for(let i = 0; i < 50;i++){
-   let tbody = document.getElementById("tbody")
-    tbody.innerHTML += "<tr>" + "<td></td>".repeat(50) + "</tr>"
+for(let i = 0; i < numOfRows;i++){
+    
+   let tbody = document.getElementById("tbody")//to bve refactored
+    tbody.innerHTML += "<tr>" + "<td></td>".repeat(numOfRows) + "</tr>"
 }
-let body = document.getElementById("tbody")
-let arr = body.children;
-console.log(arr)
-// arr[5].style
-//tbody.children[0].children[8].style.backgroundColor = "green"
-tbody.style.backgroundColor = "white" 
+let tbody = document.getElementById("tbody")
+// let arr = body.children;
+// console.log(arr)
 function delay(time){
     return new Promise(resolve => setTimeout(resolve, time));
 }
 let tempid = 1;
-for(let i = 0; i < 50;i++){
-    for(let j = 0; j < 50;j++){
+for(let i = 0; i < numOfRows;i++){
+    for(let j = 0; j < numOfRows;j++){
         let cell = tbody.children[i].children[j]
 cell.style.backgroundColor = "green"
-cell.setAttribute("id", tempid)
+cell.setAttribute("id", tempid)//Assign ID dynamically from 1 to numOfCells
 
-//______________________________________________________________________________
+//create click even for each cell______________________________________________________________________________
 const btn = document.getElementById(tempid);
 
 btn.addEventListener('click', function onClick(event) {
-  // 👇️ Change text color globally
+  // 👇️ Change cell color switching between two colors
   if(cell.style.backgroundColor == 'yellow'){
     cell.style.backgroundColor = 'green'
   }else if(cell.style.backgroundColor == 'green'){
     cell.style.backgroundColor = 'yellow'
   }
-
-  // 👇️ Change text color for clicked element only
-  // event.target.style.color = 'salmon';
 });
 //________________________________________________________
 tempid++;
 }
 }
 
-//______________________________________________________________________________
+//Create Actions for start button______________________________________________________________________________
 const start = document.getElementById("start_btn");
 
 start.addEventListener('click', function onClick(event) {
-    let cellCurrStates = []
-let cellNextStates = []
-//_________________________________________tracking states
-for(let i = 1; i <= 2500;i++){
+    // let cellCurrStates = []
+    // console.log("line 78 cell current state", cellCurrStates)
+    // let cellNextStates = []
+    
+    // console.log("line 80 cell next state", cellNextStates)
+    let num = 1;
+    let counter = 1
+   
+    function automateClick(){setTimeout(function(){
+        let cellCurrStates = []
+    console.log("line 78 cell current state", cellCurrStates)
+    let cellNextStates = []
+    
+    console.log("line 80 cell next state", cellNextStates)
+
+// instantiate tracking arrays to track current and next states
+    for(let i = 0; i < numOfCellsCurr; i++){
+        cellCurrStates[trackCurrState] = 0
+        cellNextStates[trackCurrState] = 0
+    }
+
+
+     for(let i = 1; i <= numOfCells;i++){
+    
     let temp = document.getElementById(i)
+    let trackCurrState = i + numOfRowsCurr + (Math.floor((i - 1)/numOfRows))*(2)
     if(temp.style.backgroundColor == "green"){
-        cellCurrStates.push(0)
+        cellCurrStates[trackCurrState] = 0
     }else{
-        cellCurrStates.push(1)
+        cellCurrStates[trackCurrState] = 1
     }
 }
-//setting next states
-for(let i = 1; i <= 2500;i++){
+//_________________________________________tracking current states
+
+//setting next states from current states following rules//_________________________________________
+
+for(let i = 1; i <= numOfCells;i++){
     let temp = document.getElementById(i)
     //still to account for cell at the edges
     //originally off state____________________________________________________________________________________________
-    if(cellCurrStates[i-1] == 0 && (
-        cellCurrStates[(i-1) - 51] + cellCurrStates[(i-1) - 50] + cellCurrStates[(i-1)-49] + cellCurrStates[(i-1)-1] + 
-        cellCurrStates[(i-1)+1] + cellCurrStates[(i-1)+49] + cellCurrStates[(i-1)+50] + cellCurrStates[(i-1)+51]
-    ) == 3){
-        cellNextStates.push(1)
-    }else if(cellCurrStates[i-1] == 1 && (
-        cellCurrStates[(i-1) - 51] + cellCurrStates[(i-1) - 50] + cellCurrStates[(i-1)-49] + cellCurrStates[(i-1)-1] + 
-        cellCurrStates[(i-1)+1] + cellCurrStates[(i-1)+49] + cellCurrStates[(i-1)+50] + cellCurrStates[(i-1)+51]
-    ) < 2){
-        cellNextStates.push(0)
-    }else if(cellCurrStates[i-1] == 1 && (
-        cellCurrStates[(i-1) - 51] + cellCurrStates[(i-1) - 50] + cellCurrStates[(i-1)-49] + cellCurrStates[(i-1)-1] + 
-        cellCurrStates[(i-1)+1] + cellCurrStates[(i-1)+49] + cellCurrStates[(i-1)+50] + cellCurrStates[(i-1)+51]
-    ) > 3){
-        cellNextStates.push(0)
-    }else if(cellCurrStates[i-1] == 1 && (
-        cellCurrStates[(i-1) - 51] + cellCurrStates[(i-1) - 50] + cellCurrStates[(i-1)-49] + cellCurrStates[(i-1)-1] + 
-        cellCurrStates[(i-1)+1] + cellCurrStates[(i-1)+49] + cellCurrStates[(i-1)+50] + cellCurrStates[(i-1)+51]
-    ) == 3){
-        cellNextStates.push(1)
-    }else if(cellCurrStates[i-1] == 1 && (
-        cellCurrStates[(i-1) - 51] + cellCurrStates[(i-1) - 50] + cellCurrStates[(i-1)-49] + cellCurrStates[(i-1)-1] + 
-        cellCurrStates[(i-1)+1] + cellCurrStates[(i-1)+49] + cellCurrStates[(i-1)+50] + cellCurrStates[(i-1)+51]
-    ) == 2){
-        cellNextStates.push(1)
-    }else(
-        cellNextStates.push(0)
+
+    let trackCurrState = i + numOfRowsCurr + (Math.floor((i - 1)/numOfRows))*(2)
+    let surrCellState = (
+        cellCurrStates[(trackCurrState) - (numOfRowsCurr + 1) ] + cellCurrStates[(trackCurrState) - numOfRowsCurr] + cellCurrStates[(trackCurrState)-(numOfRowsCurr - 1)] + cellCurrStates[(trackCurrState)-1] + 
+        cellCurrStates[(trackCurrState)+1] + cellCurrStates[(trackCurrState)+(numOfRowsCurr - 1)] + cellCurrStates[(trackCurrState)+numOfRowsCurr] + cellCurrStates[(trackCurrState)+(numOfRowsCurr + 1)]
+    )
+
+    if(cellCurrStates[trackCurrState] == 0 && surrCellState == 3){
+        cellNextStates[trackCurrState] = 1
+    }
+    //Any live cell with fewer than two live neighbours dies (referred to as underpopulation).//________________________________
+    else if(cellCurrStates[trackCurrState] == 1 && surrCellState < 2){
+        cellNextStates[trackCurrState] = 0
+    }
+    //Any live cell with more than three live neighbours dies (referred to as overpopulation).//________________________________
+    else if(cellCurrStates[trackCurrState] == 1 && surrCellState > 3){
+        cellNextStates[trackCurrState] = 0
+    }
+    //Any live cell with two or three live neighbours lives, unchanged, to the next generation.//______________________________
+    //refactor the suming
+    else if(cellCurrStates[trackCurrState] == 1 && surrCellState == 3){
+        cellNextStates[trackCurrState] = 1
+    }else if(cellCurrStates[trackCurrState] == 1 && surrCellState == 2){
+        cellNextStates[trackCurrState] = 1
+    }
+    //__________________________________________________________________________________________________________________________
+    else(
+        cellNextStates[trackCurrState] = 0
     )
     //___________________________________________________________________________________________________________________
 }
-//change colors
-for(let i = 1; i <= 2500;i++){
+//using Next state to change collor(applying state)
+for(let i = 1; i <= numOfCells;i++){
+    let trackCurrState = i + numOfRowsCurr + (Math.floor((i - 1)/numOfRows))*(2)
     let temp = document.getElementById(i)
-    if(cellNextStates[i-1] == 1){
+    if(cellNextStates[trackCurrState] == 1){
         temp.style.backgroundColor = "yellow"
     }else{
         temp.style.backgroundColor = "green"
     }
 }
-    console.log(cellCurrStates)
+
+    // console.log(num)
+    // num++
+    // counter++
+    // cellCurrStates = [...cellNextStates]
+    console.log("line 156 cell curr state", cellCurrStates)
+    console.log("line 156 cell next state", cellNextStates)
+    automateClick()
+    }, 500)}
+    automateClick()
 });
 
 //________________________________________________________
+
+//Math.floor(Math.random() * (max - min) + min);
+//random Action//_________________________________________________
+const random = document.getElementById("random_btn");
+
+random.addEventListener('click', function onClick(event) {
+let cellCurrStates = []
+let cellNextStates = []
+for(let i = 1; i <= numOfCells;i++){
+    let trackCurrState = i + numOfRowsCurr + (Math.floor((i - 1)/numOfRows))*(2)
+    cellNextStates[trackCurrState] = (Math.floor(Math.random() * (3 - 0) + 0))
+}
+//using Next state to change collor(applying state)
+for(let i = 1; i <= numOfCells;i++){
+    let temp = document.getElementById(i)
+    let trackCurrState = i + numOfRowsCurr + (Math.floor((i - 1)/numOfRows))*(2)
+    if(cellNextStates[trackCurrState] == 1){
+        temp.style.backgroundColor = "yellow"
+    }else{
+        temp.style.backgroundColor = "green"
+    }
+}
+    //console.log(cellCurrStates)
+});
+
+//reset Actions//________________________________________________
+const reset = document.getElementById("reset_btn");
+
+reset.addEventListener('click', function onClick(event) {
+let cellCurrStates = []
+let cellNextStates = []
+for(let i = 1; i <= numOfCells;i++){
+    let trackCurrState = i + numOfRowsCurr + (Math.floor((i - 1)/numOfRows))*(2)
+    cellNextStates[trackCurrState] = 0
+}
+//using Next state to change collor(applying state)
+for(let i = 1; i <= numOfCells;i++){
+    let trackCurrState = i + numOfRowsCurr + (Math.floor((i - 1)/numOfRows))*(2)
+    let temp = document.getElementById(i)
+    if(cellNextStates[trackCurrState] == 1){
+        temp.style.backgroundColor = "yellow"
+    }else{
+        temp.style.backgroundColor = "green"
+    }
+}
+    //console.log(cellCurrStates)
+});
